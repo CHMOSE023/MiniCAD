1106fa0096179d7a60f5217fefda32ca508a1815
CHMOSE023 <75505898@qq.com>
2026-05-14 18:38:15 +0800

    添加图标 Win32

--- src/App/Main.cpp
+++ src/App/Main.cpp
@@ -1,16 +1,30 @@
 #include "pch.h"
 #include "MainWindow.h"
-#include "Core/GeomKernel/XLine.hpp"
+
 using namespace MiniCAD;
+
+#ifdef USE_WIN32 
+
+int APIENTRY WinMain(HINSTANCE hInst, HINSTANCE, PSTR, int)
+{  
+    MainWindow mainWindow;
+    mainWindow.Initialize(L"MiniCAD", 600, 400);
+    mainWindow.Run(); 
+    return 0;
+}
+
+#else
+
 int main()
 {
-	SetConsoleOutputCP(CP_UTF8);
-	SetConsoleCP(CP_UTF8);
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8);
 
+    MainWindow mainWindow;
+    mainWindow.Initialize(L"MiniCAD", 600, 400);
+    mainWindow.Run();
 
-	MainWindow mainWindow;
-
-	mainWindow.Initialize(L"MiniCAD",600,400);
-	mainWindow.Run();
-
+    return 0;
 }
+
+#endif
